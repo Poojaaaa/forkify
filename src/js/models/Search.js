6cf1f9af2e2f376b66558fa2b0f79406e8e074f6
Poojaaaa <Pooja.gajera11@gmail.com>
2019-03-28 12:06:59 -0700

    Building the search model

---
+++ src/js/models/Search.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '4e4004e4e7e752b3b66e5dc298d598fd';
+    try {
+      const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+    } catch (error) {
+      alert(error)
+    }
+  }
+}
